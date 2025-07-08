b63f63783d0674da16ce49d908af37176c6e3f75
Nayeem <cloudcode29@gmail.com>
2025-07-08 11:26:58 +0600

    identity

--- Modern JavaScript/script.js
+++ Modern JavaScript/script.js
@@ -1,2 +1,7 @@
 // console.log("Hello, World!");
 // console.log("This is a modern JavaScript file.");
+
+const name = "Nayeem Islam";
+const age = 18;
+const identity = `My name is ${name} and I am ${age} years old.`;
+console.log(identity);
