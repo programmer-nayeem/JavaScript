183f861a436b51bdcad7641eccf41ba1e5a0e478
Nayeem <xstarpersonal@gmail.com>
2025-07-19 20:54:12 +0600

    comment is js

---
+++ Variables and Data Types/comment.js
@@ -0,0 +1,30 @@
+/*
+📝 What are Comments in JavaScript?
+==> Comments are lines in the code that are ignored by the JavaScript engine.
+They are used to explain, describe, or temporarily disable parts of code for easier understanding or debugging.
+
+JavaScript supports two types of comments:
+*/
+
+//~~~~~~~~~~~ ✅ 1. Single-Line Comments
+//            Use "//" to write a single-line comment.
+
+// This is a single-line comment
+let name = "Cloud";  // Declare a variable
+
+
+
+
+
+
+//~~~~~~~~~~~ ✅ 2. Multi-Line Comments
+//            Use "/* */" to write multi-line comments.
+
+/* 
+This is a multi-line comment.
+   
+It can span multiple lines.
+*/
+let age = 25;
+
+console.log(name , age)
